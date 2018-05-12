8669491176caa3807f3efc7efc336375e16d2ac8
Janak Hirpara <janakhirpara03@gmail.com>
2018-05-11 21:44:09 -0400

    Added file path for ChromeDriver

--- Registration.java
+++ Registration.java
@@ -3,7 +3,8 @@ public class Registration {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\sits\\selenium-java-3.9.1\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		//This is GitHub example.
 		
