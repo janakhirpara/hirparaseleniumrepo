06d5b3c94c147b36de72aa475acaa8283726f701
Janak Hirpara <janakhirpara03@gmail.com>
2018-05-11 21:27:01 -0400

    New Class Added

---
+++ Registration.java
@@ -0,0 +1,12 @@
+
+public class Registration {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		WebDriver driver = new ChromeDriver();
+		//This is GitHub example.
+		
+	}
+
+}
